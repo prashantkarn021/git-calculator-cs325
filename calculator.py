c9145f59f458f07fb7497ee6be278afde2f23b2b
Prashant Karn <168095491+prashantkarn021@users.noreply.github.com>
2026-09-09 20:13:01 -0500

    chore: update welcome banner 1

--- calculator.py
+++ calculator.py
@@ -19,6 +19,8 @@ def calculate():
     print("Addition: 5 + 3 =", add(5, 3))
     print("Subtraction: 5 - 3 =", subtract(5, 3))
     print("Multiplication: 5 * 3 =", multiply(5, 3))
+print ("Learning the round 3")
+
 
 if __name__ == "__main__":
     calculate()
